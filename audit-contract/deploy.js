f186585c5001bef94ec3c24da3feb572299d15d0
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-11-04 21:26:24 +0000

    Updated test

--- audit-contract/deploy.js
+++ audit-contract/deploy.js
@@ -2,36 +2,29 @@ const { Tezos } = require('@taquito/taquito')
 
 const fs = require("fs");
 
+const { email, password, mnemonic, secret } = JSON.parse(fs.readFileSync('./faucet.json').toString())
+
 Tezos.setProvider({ rpc: "https://api.tez.ie/rpc/babylonnet" })
 
-Tezos.importKey("peqjckge.qkrrajzs@tezos.example.org", "y4BX7qS1UE", [
-    "skate",
-    "damp",
-    "faculty",
-    "morning",
-    "bring",
-    "ridge",
-    "traffic",
-    "initial",
-    "piece",
-    "annual",
-    "give",
-    "say",
-    "wrestle",
-    "rare",
-    "ability"
-].join(" "), "7d4c8c3796fdbf4869edb5703758f0e5831f5081").then(() => {
+Tezos.importKey(email, password, mnemonic.join(" "), secret).then(async () => {
     return Tezos.contract.originate({
         code: JSON.parse(fs.readFileSync("./build/Audit.json").toString()),
         init: {
             "prim": "Pair",
             "args":
                 [[],
-                { "string": "tz1bwsEWCwSEXdRvnJxvegQZKeX5dj6oKEys" }]
+                { "string": await Tezos.signer.publicKeyHash() }]
         },
     })
 }).then((op) => {
     return op.contract()
 }).then((contract) => {
+    const detail = {
+        address: contract.address,
+        network: "https://api.tez.ie/rpc/babylonnet"
+    }
+
+    fs.writeFileSync('./deployed/latest.json', JSON.stringify(detail))
+    fs.writeFileSync(`./deployed/${contract.address}.json`, JSON.stringify(detail))
     console.log('Deployed at:', contract.address)
 })
